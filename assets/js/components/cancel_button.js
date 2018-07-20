83a1f5752c487ac2bded498991e2fb3700ae8f41
Robert Adams <radams@labzero.com>
2018-07-19 22:04:37 -0700

    Remove FontAwesome 4, use FontAwesome 5 Pro (assume available)

--- assets/js/components/cancel_button.js
+++ assets/js/components/cancel_button.js
@@ -3,7 +3,7 @@ import React from 'react'
 class CancelButton extends React.Component {
   render () {
     return (
-      <div onClick={this.props.onPress} className='thesis-button cancel'><div className='tooltip'>Discard Changes</div><i className='fa fa-remove fa-2x' /></div>
+      <div onClick={this.props.onPress} className='thesis-button cancel'><div className='tooltip'>Discard Changes</div><i className='fa fa-minus fa-2x' /></div>
     )
   }
 }
